6d155a962dd1c808a0bbc6650be776f0caa5d36e
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-10-06 23:35:03 +0530

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -63,14 +63,14 @@ const Contact = () => {
   };
 
   return (
-    <section className="c-space my-20" id="contact">
+    <section className="c-space my-20 mb-20" id="contact">
       <Toaster /> {/* Add the Toaster component here */}
 
       <div className="relative min-h-screen flex items-center justify-center flex-col">
         <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
 
         <div className="contact-container">
-          <h3 className="head-text">Let`s talk</h3>
+          <h3 className="head-text my-10">Let`s talk</h3>
           <p className="text-lg text-white-600 mt-3">
             Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
             life, I’m here to help.
